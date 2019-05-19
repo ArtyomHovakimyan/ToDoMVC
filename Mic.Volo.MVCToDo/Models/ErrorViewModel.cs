c8f594a890733d1cc431106e270e3f82ee724aa0
User <artyom.hovakimyan01@gmail.com>
2019-05-19 13:37:11 +0400

    add model todoitem

---
+++ Mic.Volo.MVCToDo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mic.Volo.MVCToDo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
